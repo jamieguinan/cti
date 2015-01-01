562f17690be8af2fc7d7f022d620b321d6fab640
Jamie Guinan <jamieguinan@gmail.com>
2015-01-01 12:24:25 -0500

    Basic functionality.

--- lenny-cam-local-yuyv.cmd
+++ lenny-cam-local-yuyv.cmd
@@ -1,2 +1,12 @@
 new V4L2Capture vc
+new SDLstuff sdl
+
+connect vc YUV422P_buffer sdl
+
+connect sdl:Keycode_msg sdl:Keycode_msg
+connect sdl:Keycode_msg_2 vc:Keycode_msg
+
 config vc device Integrated
+config vc format YUYV
+config vc size 640x480
+config vc enable 1
