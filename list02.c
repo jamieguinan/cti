b45124b8fff2a4d499a980f0d67f4e26e9ac61ee
Jamie Guinan <jamieguinan@gmail.com>
2015-01-28 13:08:49 -0500

    More experimental work.

--- list02.c
+++ list02.c
@@ -1,7 +1,16 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-void grow(void ** items,
+/* After 20+ years of programming, I think I've come up with a generic
+   list function in C that I am "happy enough" with.  The key is to
+   separate the list growth, which can be kept generic by using
+   pointers to the individual list structure members, from the item
+   assignment, which is type-specific but can be done with a simple
+   assignment in a enclosing macro that also calls the growth
+   function.
+*/
+
+void _list_grow(void ** items,
 	  int itemsize,
 	  int * available,
 	  int * count) 
@@ -23,7 +32,11 @@ void grow(void ** items,
   printf("*available=%d\n", *available);
 }
 
-#define append(item, c) { grow((void*)&(c.items), sizeof(c.items[0]), &(c.available), &(c.count)); c.items[c.count-1] = item; }
+#define list_append(item, c) { _list_grow((void*)&(c.items), sizeof(c.items[0]), &(c.available), &(c.count)); \
+  c.items[c.count-1] = item; }
+
+#define List(t) struct { t * items; int itemsize; int available; int count; }
+
 
 /* Example type definition. */
 typedef struct {
@@ -37,10 +50,9 @@ typedef struct {
 
 
 /* Same type definition, using macros. */
-#define List(t, name) struct { t * items; int itemsize; int available; int count; } name
 
 typedef struct {
-  List(int, rates);
+  List(int) rates;
 } xyz;
 
 
@@ -49,13 +61,17 @@ typedef struct {
 int main()
 {
   abc x1 = {};
-  grow((void*)&x1.rates.items, sizeof(int), &x1.rates.available, &x1.rates.count); x1.rates.items[x1.rates.count-1] = 4;
+  _list_grow((void*)&x1.rates.items, sizeof(int), &x1.rates.available, &x1.rates.count); x1.rates.items[x1.rates.count-1] = 4;
 
   xyz x2 = {};
-  append(5, x2.rates);
+  list_append(5, x2.rates);
+
+  //List(int) * x3 = malloc(sizeof(*x3));
+  //list_append(6, x3);
   
   printf("%d\n", get(x1.rates, 0));
   printf("%d\n", get(x2.rates, 0));
+  //printf("%d\n", get(x3, 0));
 
   return 0;
 }
