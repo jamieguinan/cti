4878a4f7b06dafacbf1d6592d52ded24b2d51565
Jamie Guinan <jamieguinan@gmail.com>
2015-01-28 21:48:20 -0500

    Array.h:   Add count macros.   Comment cleanups.

--- Array.h
+++ Array.h
@@ -11,13 +11,10 @@
    macro, which C++ templates solve nicely, but can be really messy in
    C, ending up with lots of duplicate code. */
 
-/* The Array() type declaration at the bottom can be used anywhere:
-   local, pointer, struct member, parameter.  Additionally, any
-   structure with the same set of member names (with the same types),
+/* Any structure with the same set of member names (with the same types),
    can be used with the accessor macros.  Furthermore, one could call
    Array_grow() directly with any set of compatible variables.  But
    they should be initialized properly before calling. */
-
 extern void Array_grow(void ** items,
 		       int itemsize,
 		       int * available,
@@ -25,14 +22,17 @@ extern void Array_grow(void ** items,
 
 #define Array_append(item, c) { Array_grow((void*)&(c.items), sizeof(c.items[0]), &(c.available), &(c.count)); \
   c.items[c.count-1] = item; }
-
 #define Array_get(c, i) (c.items[i])
+#define Array_count(c) (c.count)
+
 
 #define Array_ptr_append(item, c) { Array_grow((void*)&(c->items), sizeof(c->items[0]), &(c->available), &(c->count)); \
   c->items[c->count-1] = item; }
-
 #define Array_ptr_get(c, i) (c->items[i])
+#define Array_ptr_count(c) (c->count)
 
+/* The Array() type declaration at the bottom can be used anywhere:
+   local, pointer, struct member, parameter.  */
 #define Array(t) struct { t * items; int itemsize; int available; int count; }
 
 #endif
