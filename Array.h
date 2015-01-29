89705046d43b7f15d592bb43acdb9351473efdec
Jamie Guinan <jamieguinan@gmail.com>
2015-01-28 20:07:11 -0500

    Array.h:   Absortb comments from array02.c .
    array02.c:
      Use Array module.

--- Array.h
+++ Array.h
@@ -1,12 +1,22 @@
 #ifndef _CTI_ARRAY_H_
 #define _CTI_ARRAY_H_
 
+
+/* The thing I like about this module is that separates the list
+   growth, which can be kept generic by using pointers to the
+   individual list structure members, from the item assignment, which
+   is type-specific but can be done with a simple assignment in a
+   enclosing macro that also calls the growth function.  My instinct
+   had always been to include the growth and assignment in the same
+   macro, which C++ templates solve nicely, but can be really messy in
+   C, ending up with lots of duplicate code. */
+
 /* The Array() type declaration at the bottom can be used anywhere:
    local, pointer, struct member, parameter.  Additionally, any
-   structure with the same set of member names (with the same
-   types), can be used with the accessor macros.   Furthermore,
-   one could call Array_grow() directly with any set of compatible
-   variables. */
+   structure with the same set of member names (with the same types),
+   can be used with the accessor macros.  Furthermore, one could call
+   Array_grow() directly with any set of compatible variables.  But
+   they should be initialized properly before calling. */
 
 extern void Array_grow(void ** items,
 		       int itemsize,
--- array02.c
+++ array02.c
@@ -1,55 +1,8 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-/* After 20+ years of programming, I think I've come up with a generic
-   list function in C that I am "happy enough" with.  The key is to
-   separate the list growth, which can be kept generic by using
-   pointers to the individual list structure members, from the item
-   assignment, which is type-specific but can be done with a simple
-   assignment in a enclosing macro that also calls the growth
-   function.  My instinct had always been to include the growth
-   and assignment in the same macro, which C++ templates solve
-   nicely, but can be really messy in C, ending up with lots of
-   duplicate code.
-
-   Haha, turns out I had written very similar code in XArray.c some
-   time ago.
-*/
-
-void _array_grow(void ** items,
-	  int itemsize,
-	  int * available,
-	  int * count) 
-{
-  *count += 1;
-  //printf("*available=%d\n", *available);
-  if (*available == 0) {
-    *available = 2;
-    void *tmp = malloc(itemsize*(*available));
-    /* FIXME: if (!tmp) { error_handler(); return; } */
-    (*items) = tmp;
-  }
-  else if (*count == *available) {
-    *available *= 2;
-    void *tmp = realloc((*items), itemsize*(*available));
-    /* FIXME: if (!tmp) { error_handler(); return; } */
-    (*items) = tmp;
-  }
-  // printf("*available=%d\n", *available);
-}
-
-#define array_append(item, c) { _array_grow((void*)&(c.items), sizeof(c.items[0]), &(c.available), &(c.count)); \
-  c.items[c.count-1] = item; }
-
-#define array_get(c, i) (c.items[i])
-
-#define array_ptr_append(item, c) { _array_grow((void*)&(c->items), sizeof(c->items[0]), &(c->available), &(c->count)); \
-  c->items[c->count-1] = item; }
-
-#define array_ptr_get(c, i) (c->items[i])
-
-/* This Array type declaration can be used anywhere: local, pointer, struct member, parameter */
-#define Array(t) struct { t * items; int itemsize; int available; int count; }
+#include "Array.c"
+#include "Mem.c"
 
 /* Example type definition. */
 typedef struct {
@@ -72,19 +25,19 @@ int main()
 {
   abc x1 = {};
   /* explicit call */
-  _array_grow((void*)&x1.rates.items, sizeof(int), &x1.rates.available, &x1.rates.count); x1.rates.items[x1.rates.count-1] = 4;
+  Array_grow((void*)&x1.rates.items, sizeof(int), &x1.rates.available, &x1.rates.count); x1.rates.items[x1.rates.count-1] = 4;
 
   xyz x2 = {};
   /* macro call */
-  array_append(5, x2.rates);
+  Array_append(5, x2.rates);
 
   /* pointer declaration */
   Array(int) * x3 = malloc(sizeof(*x3));
-  array_ptr_append(6, x3);
+  Array_ptr_append(6, x3);
   
-  printf("%d\n", array_get(x1.rates, 0));
-  printf("%d\n", array_get(x2.rates, 0));
-  printf("%d\n", array_ptr_get(x3, 0));
+  printf("%d\n", Array_get(x1.rates, 0));
+  printf("%d\n", Array_get(x2.rates, 0));
+  printf("%d\n", Array_ptr_get(x3, 0));
 
   return 0;
 }
