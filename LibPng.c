e51c990bb762b329cd9aecc1290f3636fecd78ea
Jamie Guinan <jamieguinan@gmail.com>
2015-01-30 15:42:59 -0500

    Stub libpng module.

---
+++ LibPng.c
@@ -0,0 +1,8 @@
+#include "LibPng.h"
+#include "png.h"
+
+RGBA_buffer * Png_load(const char * filename)
+{
+  
+  return NULL;
+}
---
+++ LibPng.h
@@ -0,0 +1,8 @@
+#ifndef _CTI_LIBPNG_H_
+#define _CTI_LIBPNG_H_
+
+#include "Images.h"
+
+extern RGBA_buffer * Png_load(const char * filename);
+
+#endif
