c583256b3d680d7cd2403b2929d612dd57792ec6
Jamie Guinan <jamieguinan@gmail.com>
2015-07-08 02:14:30 -0400

    HTTPClient.c:   Fill in some preliminary code.
    HTTPClient.h:
      More states.
      HTTPTransaction struct.

--- HTTPClient.c
+++ HTTPClient.c
@@ -1,12 +1,25 @@
 #include "HTTPClient.h"
+#include "Mem.h"
 
 void HTTP_clear(HTTPTransaction ** t)
 {
+  if ((*t)->comm) {
+    Comm_free(&((*t)->comm));
+  }
+  Mem_free(*t);
+  *t = NULL;
 }
 
 
 void HTTP_init_get(HTTPTransaction ** t, String * url)
 {
+  if (t) {
+    fprintf(stderr, "%s: warning, stale transaction struct lost\n", __func__);
+  }
+  *t = Mem_calloc(1, sizeof(HTTPTransaction *));
+  (*t)->comm = Comm_new(s(url));
+  (*t)->code = 0;
+  (*t)->state = HTTP_STATE_GET_XMIT;
 }
 
 
@@ -17,10 +30,37 @@ void HTTP_init_put(HTTPTransaction ** t, String * url, ArrayU8 * postdata)
 
 void HTTP_process(HTTPTransaction * t)
 {
+#if 0
+  // In progress, commented out so I can work on other code.
+
+  //HTTP_STATE_NONE,
+  //HTTP_STATE_INIT,
+  //HTTP_STATE_ACTIVE,
+  //HTTP_STATE_COMPLETE,
+
+  if (t->comm->state == IO_CLOSED) {
+    t->code = 500;
+    t->state = HTTP_STATE_COMPLETE;
+    return;
+  }
+
+  if (t->state == HTTP_STATE_NONE) {
+    /* FIXME:  Error.  Sleep and return? */
+  } 
+  else if (t->state == HTTP_STATE_COMPLETE) {
+    /* Nothing to do. */
+  }
+  else if (t->state == HTTP_STATE_GET_XMIT) {
+    // ...
+  }
+  else if (t->state == HTTP_STATE_POST_XMIT) {
+    // ...
+  }
+#endif
 }
 
 
 int HTTP_state(HTTPTransaction * t)
 {
-  return 0;
+  return (t->state);
 }
--- HTTPClient.h
+++ HTTPClient.h
@@ -2,8 +2,24 @@
 #define _CTI_HTTPCLIENT_H_
 
 #include "ArrayU8.h"
+#include "SourceSink.h"
+
+typedef enum {
+  HTTP_STATE_NONE,
+  HTTP_STATE_INIT,
+  HTTP_STATE_GET_XMIT,
+  HTTP_STATE_POST_XMIT,
+  HTTP_STATE_REPLY,
+  HTTP_STATE_COMPLETE,
+} HTTP_STATE;
 
 typedef struct {
+  Comm * comm;
+  int code;			/* 200, 404, 500, whatever */
+  String * url;
+  ArrayU8 * postdata;
+  int postdata_offset;
+  HTTP_STATE state;
 } HTTPTransaction;
 
 
@@ -14,12 +30,6 @@ extern void HTTP_init_get(HTTPTransaction ** t, String * url);
 extern void HTTP_init_put(HTTPTransaction ** t, String * url, ArrayU8 * postdata);
 
 extern void HTTP_process(HTTPTransaction * t);
-
-typedef enum {
-  HTTP_STATE_INIT,
-  HTTP_STATE_ACTIVE,
-  HTTP_STATE_COMPLETE
-} HTTP_STATE;
 
 extern int HTTP_state(HTTPTransaction * t);
 
