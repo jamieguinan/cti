7cae1e8d17186e63738397e88c14d383fa76a681
Jamie Guinan <jamieguinan@gmail.com>
2015-08-31 12:19:27 -0400

    Various .cmd file updates for IP address changes.

--- aria-record.cmd
+++ aria-record.cmd
@@ -4,7 +4,7 @@ new MjpegMux mjm
 connect mjd Jpeg_buffer mjm
 connect mjd Wav_buffer mjm
 
-config mjd input 192.168.2.109:6667
+config mjd input aria.local:6667
 
 config mjm output /av/media/tmp/aria-%Y%m%d-%H%M%S.mjx
 
