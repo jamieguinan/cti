5a31ed07c6940864f23a7d510f672765db2e6ec0
Jamie Guinan <jamieguinan@gmail.com>
2015-11-24 12:59:55 -0500

    ipcxr.c: ipcxr.mk:   ipc application test program.

---
+++ ipcxr.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include "SourceSink.h"
+#include "File.h"
+#include "ipc.h"
+
+int main(int argc, char * argv[])
+{
+  unsigned int timeout_ms = 1000;
+
+  if (argc < 3) {
+    printf("Usage: %s file host:port [ timeout_seconds ]\n", argv[0]);
+    return 1;
+  }
+  
+  if (argc == 4) {
+    int seconds;
+    if (sscanf(argv[3], "%d", &seconds) == 1) {
+      timeout_ms = seconds * 1000;
+    }
+  }
+
+  ArrayU8 * msg = File_load_data(S(argv[1]));
+  
+  if (!msg) {
+    printf("Failed to load %s\n", argv[1]);
+    return 1;
+  }
+
+  Comm * c = Comm_new(argv[2]);
+  if (!IO_ok(c)) {
+    printf("connection to %s failed\n", argv[2]);
+    return 1;
+  }
+  
+  int rc;
+
+  rc =ipc_send(c->io.s, msg->data, msg->len, timeout_ms);
+  if (rc != 0) {
+    printf("ipc_send failed\n");
+    return 1;
+  }
+
+  uint8_t * response = NULL;
+  uint32_t response_length = 0;
+  ipc_recv(c->io.s, &response, &response_length, timeout_ms);
+  if (rc != 0) {
+    printf("ipc_recv failed\n");
+    return 1;
+  }
+
+  int i;
+  for (i=0; i < response_length; i++) {
+    putchar((char)response[i]);
+  }
+
+  return 0;
+}
---
+++ ipcxr.mk
@@ -0,0 +1,2 @@
+ipcxr: ipcxr.c File.c ArrayU8.c Mem.c SourceSink.c String.c ipc.c dpf.c Stats.c
+	gcc -Wall ipcxr.c File.c ArrayU8.c Mem.c SourceSink.c String.c ipc.c dpf.c Stats.c -o ipcxr
