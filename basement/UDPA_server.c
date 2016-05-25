f5da1052a421afbcbad0b100201b7b5ba044a35a
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2016-05-25 19:27:21 -0400

    basement/UDPA_server.c:   More early development. Testable with nc (netcat).

--- basement/UDPA_server.c
+++ basement/UDPA_server.c
@@ -1,9 +1,14 @@
+/* 
+ * gcc -O -Wall -Wno-unused-result UDPA_server.c  -o UDPA_server
+ */
+
 /* Client connection states. */
 #define UDPA_CLIENT_INIT       0
 #define UDPA_CLIENT_CONNECTED  1
 #define UDPA_CLIENT_CLOSED     2
 
 #include <stdio.h>		/* perror */
+#include <stdlib.h>		/* system */
 #include <unistd.h>		/* sleep */
 #include <arpa/inet.h>
 #include <sys/socket.h>
@@ -12,6 +17,8 @@
 typedef struct {
   int state;
   int udp_socket;
+  struct sockaddr_in remote;
+  int seq;
 } UDPAckServer;
 
 void UDPA_send(UDPAckServer * uas, void * pkt, int length)
@@ -26,7 +33,12 @@ void UDPA_send(UDPAckServer * uas, void * pkt, int length)
 void UDPA_accept(UDPAckServer * uas)
 {
   /* wait for initial datagram. */
-  
+  uint8_t buffer[32000];
+  unsigned int remote_len = sizeof(uas->remote);
+  ssize_t n;
+  n = recvfrom(uas->udp_socket, buffer, sizeof(buffer), 0, (struct sockaddr *) &uas->remote, &remote_len);
+  printf("%s: %d n=%zu\n", __func__, uas->seq, n);
+  uas->seq += 1;
 }
 
 void UDPA_init(UDPAckServer * uas, uint16_t port)
@@ -46,8 +58,6 @@ void UDPA_init(UDPAckServer * uas, uint16_t port)
     perror("bind");
     return;
   }
-
-  
 }
 
 
@@ -55,6 +65,13 @@ int main(int argc, char * argv[])
 {
   UDPAckServer uas = { };
   UDPA_init(&uas, 6667);
-  sleep(10);
+
+  system("netstat -uan | grep 6667");
+
+  while (1) {
+    UDPA_accept(&uas);
+  }
+
+
   return 0;
 }
