7d19cb47307924052e323b6d82e9d720c6960d1c
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2016-05-26 01:52:05 -0400

    UDPA_client.c: New file. Testing synchronous UDP protocol.

---
+++ basement/UDPA_client.c
@@ -0,0 +1,66 @@
+/* 
+   gcc -O -Wall -Wno-unused-result UDPA_client.c  -o UDPA_client
+ */
+#include <stdio.h>		/* perror */
+#include <stdlib.h>		/* system */
+#include <unistd.h>		/* sleep */
+#include <arpa/inet.h>
+#include <sys/socket.h>
+#include <stdint.h>		/* uint16_t */
+
+typedef struct {
+  int state;
+  int udp_socket;
+  struct sockaddr_in remote;
+  int seq;
+} UDPAckClient;
+
+void UDPA_client_go(UDPAckClient * uac)
+{
+  /* send initial datagram. */
+  uint8_t message[1401] = { 0 };
+  unsigned int remote_len = sizeof(uac->remote);  
+  uint8_t buffer[32000];
+  struct sockaddr_in xremote;
+  unsigned int xremote_len = sizeof(xremote);
+  ssize_t n;
+  while (1) {
+    n = sendto(uac->udp_socket, message, sizeof(message), 0, (struct sockaddr *) &uac->remote, remote_len);
+    printf("%s: %d n=%ld\n", __func__, uac->seq, n);
+    if (n == -1) {
+      perror("sendto");
+      break;
+    }
+
+    n = recvfrom(uac->udp_socket, buffer, sizeof(buffer), 0, (struct sockaddr *) &uac->remote, &remote_len);
+    printf("%s: %d n=%zu\n", __func__, uac->seq, n);
+  }
+}
+
+
+void UDPA_client_init(UDPAckClient * uac, const char * server, uint16_t port)
+{
+  uac->udp_socket = socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP);
+  if (uac->udp_socket == -1) {
+    perror("socket");
+    return;
+  }
+
+  uac->remote.sin_family = AF_INET;
+  uac->remote.sin_port = htons(port);
+
+  if (inet_aton(server, &uac->remote.sin_addr) == 0) {
+    perror("inet_aton");
+    return;
+  }
+}
+
+
+int main(int argc, char * argv[])
+{
+  UDPAckClient uac = { };
+  UDPA_client_init(&uac, "192.168.2.8", 6667);
+  UDPA_client_go(&uac);
+
+  return 0;
+}
