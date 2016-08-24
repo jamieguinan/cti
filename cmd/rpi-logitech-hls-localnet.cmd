aa152d05623081f090db4e6f550d20cb4b10d580
Jamie Guinan (rpi2n1) <jamieguinan@gmail.com>
2016-08-24 19:17:11 +0000

    cmd/rpi-logitech-hls-localnet.cmd:   Use longer name for ts files.   Set same chunk duration for muxer and encoder.   Try different post path.   Lower bit rate.

--- cmd/rpi-logitech-hls-localnet.cmd
+++ cmd/rpi-logitech-hls-localnet.cmd
@@ -19,9 +19,8 @@ connect aenc AAC_buffer tsm
 config tsm pmt_pcrpid 258
 config tsm pmt_essd 0:15:257
 config tsm pmt_essd 1:27:258
-config tsm duration 15
 config tsm index_dir /dev/shm/hls1
-config tsm output /dev/shm/hls1/test%s.ts
+config tsm output /dev/shm/hls1/frontporch-%Y%m%d-%H%M%S.ts
 
 config vc device /dev/video0
 config vc format YUYV
@@ -30,7 +29,12 @@ config vc fps 15
 config vc autoexpose 3
 
 config enc fps 15
-config enc bitrate 500000
+config enc bitrate 300000
+
+# These 2 need to be the same.
+# FIXME: Could share a config key somehow?
+config enc gop_seconds 15
+config tsm duration 15
 
 config ac device U0x46d0x81b
 config ac rate 16000
@@ -38,7 +42,7 @@ config ac channels 1
 config ac format signed.16-bit.little.endian
 config ac frames_per_io 256
 
-config pq uri http://nucf696.local/ws/postdata
+config pq uri http://nucf696.local/cgi-bin/ws/postdata
 config pq service_key /home/pi/etc/hls_service_key.nucf696
 
 connect tsm Push_data pq
