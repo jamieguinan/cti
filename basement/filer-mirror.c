4119a3abff211a2666aba5c52effb3fb1ce5f996
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2016-11-27 13:19:13 -0500

    basement/filer-mirror.c:   Add description comment.   Fill in get_file().   Usable, but currently hard-coded for initial test IP address.

--- basement/filer-mirror.c
+++ basement/filer-mirror.c
@@ -1,11 +1,25 @@
+/*
+ * This program mirrors files from the "Filer" iOS app, which has a
+ * built-in HTTP server.
+ *
+ *   http://hammockdistrict.com/filer
+ *
+ * Filer is my favorite iOS app. It provides a simple file
+ * that you can put stuff in. No frills.
+ */
+
 #include "String.h"
 #include "Regex.h"
 #include "serf_get.h"
 #include "localptr.h"
 #include <stdio.h>
 
-void get_file(String * prefix, String * path)
+void get_file(String * host_port, String * path)
 {
+  localptr(String, outfile) = String_sprintf(".%s", s(path));
+  localptr(String, url) = String_sprintf("%s%s", s(host_port), s(path));
+  printf("Getting %s to %s\n", s(url), s(outfile));  
+  //serf_command_get(S("serf_get"), url, outfile);
 }
 
 void get_dir(String * host_port, String * path)
@@ -14,14 +28,15 @@ void get_dir(String * host_port, String * path)
   localptr(String, result) = String_new("");
   localptr(String, url) = String_sprintf("%s%s", s(host_port), s(path));
   serf_command_get(S("serf_get"), url, result);
-  localptr(String, result2) = String_replace(result, "\n", "");
-  localptr(String_list, matches) = Regex_get_matches(s(result2), "<a.href=\"([^\"]+)\"");
+  localptr(String_list, matches) = Regex_get_matches(s(result), "<a.href=\"([^\"]+)\"");
   for (i=0; i < String_list_len(matches); i++) {
     String * href = String_list_get(matches, i);
     if (String_eq(href, S(".."))) { continue; }
-    printf("%s\n", s(href));
     if (String_ends_with(href, "/")) {
       get_dir(host_port, href);
+    }
+    else {
+      get_file(host_port, href);      
     }
   }
 }
