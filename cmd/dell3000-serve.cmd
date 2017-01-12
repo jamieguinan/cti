ea88c89b052fe96feaf4f7e693c388e2e747b547
Jamie <guinan@dell3000.home>
2017-01-12 03:50:33 -0500

    cmd/dell3000-serve.cmd:   Serve MJX streams.

---
+++ cmd/dell3000-serve.cmd
@@ -0,0 +1,32 @@
+new V4L2Capture vc
+new CJpeg cj
+new MjpegMux mjm
+new SocketServer ss
+new ALSACapture ac
+
+connect vc BGR3_buffer cj
+connect cj Jpeg_buffer mjm
+connect ac Wav_buffer mjm
+connect mjm RawData_buffer ss
+
+config ss v4port 5103
+config ss enable 1
+
+config vc device BT878
+config vc format BGR3
+config vc std NTSC
+config vc size 640x480
+config vc input Composite1
+config vc mute 0
+#config vc Contrast 63
+config vc enable 1
+
+system amixer sset 'Line' 0%,0% cap
+system amixer sset 'Capture' 80%,80% cap
+
+config ac device default
+config ac rate 48000
+config ac channels 2
+config ac format signed.16-bit.little.endian
+config ac frames_per_io 512
+config ac enable 1
