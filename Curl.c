8d12be3a21e97884c315b8f973126e0735ec5e28
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2018-01-23 17:51:06 -0500

    Curl.c: Curl.h: libcurl wrapper, stub files.

---
+++ Curl.c
@@ -0,0 +1,11 @@
+/* libcurl wrapper */
+
+#include <curl/curl.h>
+
+static CURL * curl;
+
+
+void Curl_init(void)
+{
+}
+
---
+++ Curl.h
@@ -0,0 +1,6 @@
+#ifndef _CTI_CURL_H_
+#define _CTI_CURL_H_
+
+extern void Curl_init(void);
+
+#endif
