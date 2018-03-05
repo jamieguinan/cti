698e5e0bc95eb0411c4cfde1f74fecb9dd0aae7a
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2018-03-05 13:17:47 -0500

    cmd/compositor-test.cmd: More test cases.

--- cmd/compositor-test.cmd
+++ cmd/compositor-test.cmd
@@ -4,6 +4,8 @@ new CJpeg cj
 new JpegFiler jf
 new Compositor cs
 
+system rm -vf cstest*jpg
+
 connect js Jpeg_buffer dj
 connect dj YUV420P_buffer cs
 connect cs YUV420P_buffer cj
@@ -11,19 +13,159 @@ connect cj Jpeg_buffer jf
 
 config jf prefix cstest-
 
-config cs size 1280x640
-config cs require Test1,Test2,Test3
-config cs paste Test1:0,0,640,360:0,640:270
-config cs paste Test2:0,0,640,360:920,640:270
-config cs paste Test3:40,0,560,360:920,640:180
+config cj quality 99
+
+config cs size 1280x720
+config cs require Test1
+config cs paste Test1:0,0,400,300:0,0:0
 
 config js label Test1
-config js file Compositor-test01.jpg
+config js file basement/red-arrow-1px-edge.jpg
 config js run 1
 
-config js label Test2
+# Rotate 0
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:2,2:0
 config js run 1
 
-config js file Compositor-test02.jpg
-config js label Test3
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:4,4:0
 config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:-2,-2:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:-190,-140:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:-398,0:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:0,-298:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:-400,-300:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1278,0:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:0,718:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1278,718:0
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1280,720:0
+config js run 1
+
+
+# Rotate 90
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:300,0:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:2,0:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:300,718:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1280,0:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1380,0:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1480,0:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1530,0:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1578,0:90
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:1578,718:90
+config js run 1
+
+# Rotate 180
+system sleep 0.25
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:2,2:180
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:402,2:180
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:400,300:180
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:402,302:180
+config js run 1
+
+
+# Rotate 270
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:0,720:270
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:-298,720:270
+config js run 1
+
+system sleep 0.25
+config cs clear 1
+config cs paste Test1:0,0,400,300:200,150:270
+config js run 1
+
+system sleep 0.25
+exit
+
