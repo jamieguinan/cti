1855fc17373481ea37e06e31b5c47c66b33bad03
Jamie Guinan (rpi) <jamieguinan@gmail.com>
2018-03-08 20:52:44 +0000

    CTI.c: Tiny whitespace fix.

--- CTI.c
+++ CTI.c
@@ -177,7 +177,7 @@ Handler_message *GetData(Instance *pi, int wait_flag)
      So I am leaving the lock acquire/release.  Links,
 
      http://infocenter.arm.com/help/index.jsp?topic=/com.arm.doc.dui0472c/CJAEGDEA.html
-     http://iphonedevsdk.com/forum/iphone-sdk-development/115122-multi-core-arm-and-sharing-global-variable-between-threads.html     
+     http://iphonedevsdk.com/forum/iphone-sdk-development/115122-multi-core-arm-and-sharing-global-variable-between-threads.html
 
   */
 
