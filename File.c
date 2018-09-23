1a030ba099ef07ab431da8ff696640956524754b
Jamie Guinan (nucf696) <jamieguinan@gmail.com>
2018-09-23 04:38:20 -0400

    Revert "File.c:"
    This reverts commit 23368805dbaaaf8eb7f8906138db3113d9755ffe.

--- File.c
+++ File.c
@@ -31,7 +31,7 @@ ArrayU8 * File_load_data(String * filename)
   }
 
   ArrayU8 *a = ArrayU8_new();
-  ArrayU8_extend(a, len+1);     /* +1 to allow for [n]=0 below */
+  ArrayU8_extend(a, len);
   n = fread(a->data, 1, len, f);
   if (n < len && !procflag) {
     fprintf(stderr, "warning: only read %ld of %ld expected bytes from %s\n", n, len, s(filename));
