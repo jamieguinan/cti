a250a46a814aedd1cc2c83e6f02ba1f49b39782d
Jamie Guinan <jamieguinan@gmail.com>
2020-01-23 15:48:12 -0500

    Update jpegprof-100x.cmd.

--- cmd/jpegprof-100x.cmd
+++ cmd/jpegprof-100x.cmd
@@ -3,11 +3,22 @@ new DJpeg dj
 new Null null 
 
 config dj dct_method ifast
-config js file cti/x1.jpg
+config js file cmd/x1.jpg
 connect js Jpeg_buffer dj
 connect dj YUV422P_buffer null
 connect js Config_msg dj
 
-# v 1
+# Don't want extra images floating around in the pipeline.
+g_synchronous
+
 config js run 100
+
+# Connect to a CJpeg->JpegFiler chain to verify that
+# everything is working.
+new CJpeg cj
+new JpegFiler jf
+config jf prefix cmd/
+connect dj YUV422P_buffer cj
+connect cj Jpeg_buffer jf
+config js run 1
 
